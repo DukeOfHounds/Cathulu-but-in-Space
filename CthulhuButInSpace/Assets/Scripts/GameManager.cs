24dee8032d97f9a0497a3b9d4a845fefef22367f
Connor Hartzog <hartzogcr@gmail.com>
2021-09-11 18:19:19 -0700

    Re-Imported Content

---
+++ CthulhuButInSpace/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("=== Menue Toggles ===")]
+    [SerializeField]
+    private bool gameOver = false;
+
+    [Header("=== Menue Toggles ===")]
+    [SerializeField]
+    private float timeDelay = 2f;
+
+    public GameObject gameOverUI;
+
+    private void Update()
+    {
+        if (gameOver == true)
+        {
+            Invoke("DisplayGameOver", timeDelay);
+        }
+        if (gameOver == false)
+        {
+
+            Invoke("CloseGameOver", timeDelay);
+
+        }
+
+    }
+    public void GameOver()
+    {
+        if (gameOver == false)
+        {
+            gameOver = true;
+            Debug.Log("GAME OVER");
+           
+        }
+        
+    }
+    private void DisplayGameOver()// sets Game Over screen to visible 
+    {
+        
+            gameOverUI.SetActive(true);
+        
+    }
+    private void CloseGameOver()// sets Game Over screen to visible 
+    {
+
+        gameOverUI.SetActive(false);
+
+    }
+    public void Restart()// Reloads current Scene
+    {
+        gameOverUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
